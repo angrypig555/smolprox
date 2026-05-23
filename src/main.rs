58fc2a41a22a2d363cf4882c3bcb444d81c37175
angrypig555 <brny@brny.dev>
2026-05-23 08:35:22 +0100

    add more error handling

--- src/main.rs
+++ src/main.rs
@@ -23,39 +23,44 @@ async fn main() {
 
 async fn process(mut stream: TcpStream) -> Result<()>{
     let mut buffer = [0; 1024];
-    match stream.read(&mut buffer).await {
-        Ok(n) => {
-            let request = String::from_utf8_lossy(&buffer[..n]);
-            let request_line = request.lines().next().unwrap_or("");
-            let parts: Vec<&str> = request_line.split_whitespace().collect();
-            if parts.len() < 2 {
-                let response = format!(
-                    "HTTP/1.1 400 Bad Request\r\nServer: smolprox\r\nContent-Type: text/plain\r\nContent-Length: 11\r\nConnection: close\r\n\r\nBad request",
-                );
-                stream.write_all(response.as_bytes()).await.expect("Failed to write to socket");
-                log(Severity::WARNING,"less than 2 parts in header, 400");
-            }
-            let method = parts[0];
-            if method != "CONNECT" {
-                let response = "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: 0\r\nServer: smolprox\r\nAllow: CONNECT\r\nConnection: close\r\n\r\n";
-                stream.write_all(response.as_bytes()).await.expect("Failed to write to socket");
-                log(Severity::WARNING, "Client attempted to use unsupported method");
-            }
-            let target = parts[1];
-            log(Severity::INFO, &format!("Client connecting to {}", target));
-            let target_stream = TcpStream::connect(target).await.expect("Failed to connect to target");
-            let connected_response = "HTTP/1.1 200 Connection Established\r\nProxy-Agent: smolprox/1.0\r\n\r\n";
-            stream.write_all(connected_response.as_bytes()).await.expect("Failed to write to socket");
-            let (mut client_reader, mut client_writer) = stream.into_split();
-            let (mut target_reader, mut target_writer) = target_stream.into_split();
-            let client_to_target = tokio::spawn(async move {
-                let _ = copy(&mut client_reader, &mut target_writer).await;
-            });
-            let _ = copy(&mut target_reader, &mut client_writer).await;
-            let _ = client_to_target.await;
-        }
-        Err(e) => {
-            eprintln!("socket error: {}", e);
-        }
+    let n = stream.read(&mut buffer).await?;
+    let request = String::from_utf8_lossy(&buffer[..n]);
+    let request_line = request.lines().next().unwrap_or("");
+    let parts: Vec<&str> = request_line.split_whitespace().collect();
+    if parts.len() < 2 {
+        let response = format!(
+            "HTTP/1.1 400 Bad Request\r\nServer: smolprox\r\nContent-Type: text/plain\r\nContent-Length: 11\r\nConnection: close\r\n\r\nBad request",
+        );
+        stream.write_all(response.as_bytes()).await?;
+        log(Severity::WARNING,"less than 2 parts in header, 400");
+        return Err(Error::new(ErrorKind::InvalidData, "less than 2 parts in header, 400 bad request"));
     }
+    let method = parts[0];
+    if method != "CONNECT" {
+        let response = "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: 0\r\nServer: smolprox\r\nAllow: CONNECT\r\nConnection: close\r\n\r\n";
+        stream.write_all(response.as_bytes()).await?;
+        log(Severity::WARNING, "Client attempted to use unsupported method");
+        return Err(Error::new(ErrorKind::InvalidData, "Unsupported method"));
+    }
+    let target = parts[1];
+    log(Severity::INFO, &format!("Client connecting to {}", target));
+    let mut target_stream = match TcpStream::connect(target).await {
+        Ok(stream) => stream,
+        Err(e) => {
+            log(Severity::CRITICAL, &format!("{} is unreachable: {}", target, e));
+            let response = "HTTP/1.1 502 Bad Gateway\r\nServer: smolprox\r\nContent-Type: text/plain\r\nContent-Length: 32\r\nConnection: close\r\n\r\nProxy Error: Target unreachable";
+            let _ = stream.write_all(response.as_bytes()).await;
+            return Err(e);
+        }
+    };
+    let connected_response = "HTTP/1.1 200 Connection Established\r\nProxy-Agent: smolprox/1.0\r\n\r\n";
+    stream.write_all(connected_response.as_bytes()).await?;
+    let (mut client_reader, mut client_writer) = stream.into_split();
+    let (mut target_reader, mut target_writer) = target_stream.into_split();
+    let client_to_target = tokio::spawn(async move {
+    let _ = copy(&mut client_reader, &mut target_writer).await;
+    });
+    let _ = copy(&mut target_reader, &mut client_writer).await;
+    let _ = client_to_target.await;
+    Ok(())    
 }
